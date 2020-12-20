c661f4f7b4a09e55d14023f5b50ff34e3459d86a
shengq <shengjiong007@outllok.com>
2020-12-20 18:10:50 +0800

    feat: mysql 连接

---
+++ blog-1/src/db/mysql.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql')
+const { MYSQL_CONF } = require('../conf/db.js')
+
+// 创建连接对象
+const con = mysql.createConnection(MYSQL_CONF)
+
+con.connect() // 开始链接
+
+// 统一执行 sql 函数
+function exec(sql) {
+  return new Promise((resolve, reject) => {
+    con.query(sql, (err, result) => {
+      if (err) {
+        reject(error)
+        return
+      }
+      resolve(result)
+    })
+  })
+}
+
+module.exports = {
+  exec,
+}
